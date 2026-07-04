dcb80da87be9ddd3d6ac36cd6ee6ffc6cc34bc37
nasir559 <nasiruddinratul55@gmail.com>
2026-07-04 20:21:33 +0600

    Made the buttons working and took the help of AI for Shop Now button

---
+++ all.js
@@ -0,0 +1,8 @@
+
+function shop() {
+  window.location.href = "product.html"; //Took help from AI
+}
+
+function feedback() {
+  document.getElementById("tgid").value = "Thank you for your valuable response.";
+}
